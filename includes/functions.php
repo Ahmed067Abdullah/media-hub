b12a9c5c87bf5c6fb1327bec40777f65f500197f
Ahmed Abdullah <ahmed067abdullah@gmail.com>
2018-08-09 14:26:51 +0500

    Database connected

--- includes/functions.php
+++ includes/functions.php
@@ -1,127 +1,108 @@
 <?php
 
+require_once dirname(__FILE__) . '/db.php';
+
+function runQuery($query){
+    global $connection;
+    $queryResult = mysqli_query($connection, $query);
+    if (!$queryResult) {
+        die('Error in querying database ' . mysqli_error($connection));
+    }
+    return $queryResult;
+}
+
+function dataFetched($queryResult){
+    return (mysqli_num_rows($queryResult) != 0);
+}
+
+function fetchData($queryResult){
+    return (mysqli_fetch_assoc($queryResult));
+}
+
 function showGames($count = null){
-    $limit = 14;
+    $limit = '';
     if(isset($count))
-        $limit = $count;
-    $counter = 0;
-    while($counter < $limit){
-        $game = <<<GAME
-        <div class="col-md-2">
-            <div class="item">
-                <div class="item-inner">
-                    <div class="item-img">
-                        <a href="#">
-                            <img src="img/Call-of-Duty-Modern-Warfare.jpg" alt="Call of Duty Modern Warfare" title="Call of Duty Modern Warfare"/>
-                        </a>
-                    </div>
-                    <div class="item-info">
-                        <a href="#"><h1>Call of Duty Modern Warfare</h1></a>
-                        <p><b>Platform: </b>PC</p>
-                        <p><b>Release Date: </b>22 Nov 2016</p>
-                        <p><b>Language:</b> </strong>English</p>
-                        <p><b>Genre: </b>Shooter</p>           
-                    </div>
-                </div>
-            </div>
-        </div>
-GAME;
-    echo $game;
-    $counter++;
+        $limit = 'LIMIT 5';
+    $games = runQuery("SELECT * FROM games ORDER by id DESC {$limit} ");
+
+    while($row = fetchData($games)){
+        renderStyledDiv($row['image'],$row['name'],$row['platform'],$row['release_date'],$row['language'],$row['genre'],0);
     }
 } 
 
 function showMovies($count = null){
-    $limit = 12;
+    $limit = '';
     if(isset($count))
-        $limit = $count;
-    $counter = 0;
-    while($counter < $limit){
-        $movie = <<<MOVIE
-        <div class="col-md-2">
-            <div class="item">
-                <div class="item-inner">
-                    <div class="item-img">
-                        <a href="single-item.php">
-                            <img src="img/star-wars.jpg" alt="Call of Duty Modern Warfare" title="Call of Duty Modern Warfare"/>
-                        </a>
-                    </div>
-                    <div class="item-info">
-                        <a href="single-item.php"><h1>Star Wars: The Force Awakens</h1></a>
-                        <p><b>Platform: </b>PC</p>
-                        <p><b>Release Date: </b>22 Nov 2016</p>
-                        <p><b>Language:</b> </strong>English</p>
-                        <p><b>Genre: </b>Shooter</p>        
-                    </div>
-                </div>
-            </div>
-        </div>
-MOVIE;
-    echo $movie;
-    $counter++;
+        $limit = 'LIMIT 5';
+    $games = runQuery("SELECT * FROM movies ORDER by id DESC {$limit} ");
+
+    while($row = fetchData($games)){
+        renderStyledDiv($row['image'],$row['name'],$row['lead_role'],$row['release_date'],$row['language'],$row['genre'],1);
     }
 }
 
 function showBooks($count = null){
-    $limit = 11;
+    $limit = '';
     if(isset($count))
-        $limit = $count;
-    $counter = 0;
-    while($counter < $limit){
-        $book = <<<BOOK
-        <div class="col-md-2">
-            <div class="item">
-                <div class="item-inner">
-                    <div class="item-img">
-                        <a href="#">
-                            <img src="img/book-cover.jpg" alt="Call of Duty Modern Warfare" title="Call of Duty Modern Warfare"/>
-                        </a>
-                    </div>
-                    <div class="item-info">
-                        <a href="#"><h1>Breaking the Time Barrier</h1></a>
-                        <p><b>Platform: </b>PC</p>
-                        <p><b>Release Date: </b>22 Nov 2016</p>
-                        <p><b>Language:</b> </strong>English</p>
-                        <p><b>Genre: </b>Shooter</p>   
-                    </div>
-                </div>
-            </div>
-        </div>
-BOOK;
-        echo $book;
-        $counter++;
+        $limit = 'LIMIT 5';
+    $games = runQuery("SELECT * FROM books ORDER by id DESC {$limit} ");
+
+    while($row = fetchData($games)){
+        renderStyledDiv($row['image'],$row['name'],$row['author'],$row['release_date'],$row['language'],$row['genre'],2);
     }
 }
 
 function showSoftwares($count = null){
-    $limit = 10;
+    $limit = '';
     if(isset($count))
-        $limit = $count;
-    $counter = 0;
-    while($counter < $limit){
-        $software = <<<SOFTWARE
-        <div class="col-md-2">
-            <div class="item">
-                <div class="item-inner">
-                    <div class="item-img">
-                        <a href="#">
-                            <img src="img/windows-10r.jpg" alt="Call of Duty Modern Warfare" title="Call of Duty Modern Warfare"/>
-                        </a>
-                    </div>
-                    <div class="item-info">
-                        <a href="#"><h1>Windows 10</h1></a>
-                        <p><b>Platform: </b>PC</p>
-                        <p><b>Release Date: </b>22 Nov 2016</p>
-                        <p><b>Language:</b> </strong>English</p>
-                        <p><b>Genre: </b>Shooter</p>          
-                    </div>
+        $limit = 'LIMIT 5';
+    $games = runQuery("SELECT * FROM softwares ORDER by id DESC {$limit} ");
+
+    while($row = fetchData($games)){
+        renderStyledDiv($row['image'],$row['name'],$row['platform'],$row['release_date'],$row['language'],$row['developed_by'],3);
+    }
+}
+
+//  TYPES
+// 0 => Games
+// 1 => Movies
+// 2 => Books 
+// 3 => Softwares
+
+function renderStyledDiv($image,$name,$platform,$release_date,$language,$genre,$type){
+    if($type == 1)
+        $conflict1 = "Lead Role";
+    else if($type == 2)
+        $conflict1 = "Author";
+    else
+        $conflict1 = "Platform";
+        
+    if($type == 3)
+        $conflict2 = "Developed By";
+    else
+    $conflict2 = "Genre";
+    
+    $content = <<<CONTENT
+    <div class="col-md-2">
+        <div class="item">
+            <div class="item-inner">
+                <div class="item-img">
+                    <a href="#">
+                        <img src="{$image}" />
+                    </a>
+                </div>
+            
+                <div class="item-info">
+                    <a href="#"><h1>{$name}</h1></a>
+                    <p><b>{$conflict1}: </b>{$platform}</p>
+                    <p><b>Release Date: </b>{$release_date}</p>
+                    <p><b>Language:</b> </strong>{$language}</p>
+                    <p><b>{$conflict2}: </b>{$genre}</p>           
                 </div>
             </div>
         </div>
-SOFTWARE;
-    echo $software;
-    $counter++;
+    </div>
+CONTENT;
+    echo $content;
 }
-}
-
 ?>
