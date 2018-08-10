4436c911e1087e2d3f548760c53d59435ffa7d18
Ahmed Abdullah <ahmed067abdullah@gmail.com>
2018-08-10 19:52:54 +0500

    User feedback added

--- contact.php
+++ contact.php
@@ -1,18 +1,29 @@
 <?php require "./includes/header.php" ?>
 
+<?php
+    if(isset($_POST['submit'])){
+      $name = $_POST['nam'];
+      $phone = $_POST['phone'];
+      $email = $_POST['email'];
+      $message = $_POST['message'];
+      runQuery("INSERT into users_feedback (name,phone,email,message) VALUES ('$name','$phone','$email','$message')");  
+    }
+?>
+
 <div class="contact-page container-fluid">
     <div class="container">
 	    <h1>Contact Us</h1>
             
         <div class="contact-section"> 
             <div class="contact-form">
-                <form>
-                	<input type="text" placeholder="Name">
-                	<input type="text" placeholder="Telephone">
-                    <input type="email" placeholder="Email Address">
-                    <textarea placeholder="Enter your details and message"></textarea>
-                    <button class="btn-sub">SUBMIT</button>
+                <form action = "contact.php" method="POST">
+                	<input type="text" placeholder="Name" name = "nam" required> 
+                	<input type="text" placeholder="Telephone" name = "phone" required>
+                    <input type="email" placeholder="Email Address" name = "email" required>
+                    <textarea placeholder="Enter your details and message" name = "message" required></textarea>
+                    <button class="btn-sub" name = "submit">SUBMIT</button>
                 </form>
+                <?php if(isset($_POST['submit'])) echo "<p>Form Submitted</p>"?>
             </div>
                 
             <div class="contact-detail">
@@ -21,13 +32,8 @@
                 <p><b>Mobile No:</b> 0333-8438617</p>
                 <p><b>E-mail:</b> <a href="#">hello@downloadcentrepro.com</a></p>
             </div>
-            
-        </div>
-            
-        <div class="contact-map">
-            <iframe style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d462805.0881030352!2d66.78451739952128!3d25.011505194052102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33e06651d4bbf%3A0x9cf92f44555a0c23!2sKarachi%2C+Pakistan!5e0!3m2!1sen!2s!4v1466763008717" width="600" height="450" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
         </div>
     </div>
 </div>
-
+<br><br><br><br><br><br><br><br><br><br><br><br><br>
 <?php require "./includes/footer.php" ?>
